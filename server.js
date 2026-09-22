5a69e1cce2860b10411fcb1a4c10b4252fcde941
AimanIqbal472 <mughalaiman487@gmail.com>
2026-09-22 14:56:44 +0500

    Enhance CORS configuration in server.js
    Updated CORS middleware to specify allowed origins and methods.

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+
 import connectDB from "./config/db.js";
+
 import authRoutes from "./routes/authRoutes.js";
 import noteRoutes from "./routes/noteRoutes.js";
 import errorMiddleware from "./middleware/errorMiddleware.js";
@@ -11,7 +13,16 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(
+    cors({
+        origin: [
+            "http://127.0.0.1:5500",
+            "http://localhost:5500"
+        ],
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"]
+    })
+);
 app.use(express.json());
 
 // Database
